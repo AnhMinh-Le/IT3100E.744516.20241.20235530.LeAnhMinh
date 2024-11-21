fce30c5e46e0126bfc1819f43486ffd1279f926e
Anh Minh Le <anhminhle.dsai@gmail.com>
2024-11-21 14:45:30 +0700

    update Cart class and create Cart-test

--- Lab02/AimsProject/src/AimsPackage/Cart.java
+++ Lab02/AimsProject/src/AimsPackage/Cart.java
@@ -1,18 +1,15 @@
 package AimsPackage;
 
 public class Cart {
-    // EX9
     public static final int MAX_NUMBERS_ORDERED = 20;
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private int qtyOrdered = 0;
 
-    // Method 1: Add a single DVD
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
         if (this.qtyOrdered >= MAX_NUMBERS_ORDERED) {
             System.out.println("The cart is full. Please remove some discs to continue.");
             return;
         }
-
         itemsOrdered[qtyOrdered] = disc;
         qtyOrdered++;
         System.out.println("The disc has been added");
@@ -22,7 +19,6 @@ public class Cart {
         }
     }
 
-    // Method 2: Add multiple DVDs using an array parameter
     public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
         for (DigitalVideoDisc disc : dvdList) {
             if (this.qtyOrdered >= MAX_NUMBERS_ORDERED) {
@@ -38,7 +34,6 @@ public class Cart {
         }
     }
 
-    // Method 3: Add multiple DVDs using variable arguments
     public void addDigitalVideoDisc(DigitalVideoDisc... dvds) {
         for (DigitalVideoDisc disc : dvds) {
             if (this.qtyOrdered >= MAX_NUMBERS_ORDERED) {
@@ -54,19 +49,16 @@ public class Cart {
         }
     }
 
-    // Method 4: Add two DVDs
     public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
-        addDigitalVideoDisc(dvd1); // Add first disc
-        addDigitalVideoDisc(dvd2); // Add second disc
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc(dvd2);
     }
 
-    // Remove a DVD
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         if (qtyOrdered == 0) {
             System.out.println("The cart is empty.");
             return;
         }
-
         for (int i = 0; i < qtyOrdered; ++i) {
             if (itemsOrdered[i] == disc) {
                 System.out.println("The disc with title \"" + disc.getTitle() + "\" has been removed.");
@@ -81,12 +73,50 @@ public class Cart {
         System.out.println("The disc with title \"" + disc.getTitle() + "\" is not found in the cart.");
     }
 
-    // Calculate total cost
     public float totalCost() {
         float sum = 0;
         for (int i = 0; i < qtyOrdered; i++) {
             sum += itemsOrdered[i].getCost();
         }
         return sum;
+    }
+
+    public void printCart() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+        for (int i = 0; i < qtyOrdered; i++) {
+            DigitalVideoDisc disc = itemsOrdered[i];
+            System.out.println((i + 1) + ". DVD - " + disc.getTitle() + " - " + disc.getCategory() + " - " +
+                    disc.getDirector() + " - " + disc.getLength() + ": " + disc.getCost() + " $");
+        }
+        System.out.println("Total cost: " + totalCost());
+        System.out.println("***************************************************");
+    }
+
+    public void searchById(int id) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                System.out.println("DVD found: " + itemsOrdered[i].getTitle());
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("No DVD with ID " + id + " found in the cart.");
+        }
+    }
+
+    public void searchByTitle(String title) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                System.out.println("DVD found: " + itemsOrdered[i].getTitle());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No DVD with title \"" + title + "\" found in the cart.");
+        }
     }
 }
