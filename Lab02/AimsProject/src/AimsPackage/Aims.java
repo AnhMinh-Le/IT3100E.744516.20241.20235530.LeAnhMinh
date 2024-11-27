3b9df3ea0c18c8ce1b0fccddcdf0ac494c865b8c
Anh Minh Le <anhminhle.dsai@gmail.com>
2024-11-27 22:45:42 +0700

    change code

--- Lab02/AimsProject/src/AimsPackage/Aims.java
+++ Lab02/AimsProject/src/AimsPackage/Aims.java
@@ -1,29 +1,21 @@
-package AimsPackage;
-
-//EX5
 public class Aims {
-	public static void main(String[] args) {
-		//EX10
-		//Create a new cart
-		Cart anOrder = new Cart();
-		
-		//Create new DVD objects and add them to the cart
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "RogerAllers", 87, 19.95f);
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Was", "Science Fiction", "George Lucas", 87, 24.95f);
-		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		
-		anOrder.addDigitalVideoDisc(dvd1);
-		anOrder.addDigitalVideoDisc(dvd2);
-		anOrder.addDigitalVideoDisc(dvd3);
-		
-		//EX11
-        anOrder.removeDigitalVideoDisc(dvd1);
-        anOrder.removeDigitalVideoDisc(dvd3);
-		
-		
-		//print total cost of the items in the cart
-		System.out.println("Total cost is: ");
-		System.out.println(anOrder.totalCost());
-	}
+ 
+    public static void main(String[] args) {
+     
+        Cart anOder = new Cart();
+        
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f); 
+        anOder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Science Fiction", "George Lucas", 87, 25.95f); 
+        anOder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOder.addDigitalVideoDisc(dvd3);
+
+        System.out.println("Total cost is: ");
+        System.out.println(anOder.totalCost());
+
+    }
 
 }
