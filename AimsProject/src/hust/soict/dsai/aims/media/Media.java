0ad401683e5f63441019edd23a5f06c1138e06ba
Anh Minh Le <anhminhle.dsai@gmail.com>
2024-12-04 18:55:34 +0700

    update files

--- AimsProject/src/hust/soict/dsai/aims/media/Media.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Media.java
@@ -27,15 +27,12 @@ public abstract class Media {
     public int getId() {
         return id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public String getCategory() {
         return category;
     }
-
     public float getCost() {
         return cost;
     }
@@ -43,9 +40,21 @@ public abstract class Media {
     public void setTitle(String title) {
         this.title = title;
     }
-
+    
     public boolean isMatch(String title) {
         return this.getTitle().toLowerCase().contains(title.toLowerCase());
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Media)) {
+            return false;
+        }
+        return ((Media)obj).getTitle() == this.getTitle();
+
+
+    }
 }
